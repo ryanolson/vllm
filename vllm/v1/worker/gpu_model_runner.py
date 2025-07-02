50c80f855688b53339a2d5a263ef8905ae1fc657
jthomson04 <jwillthomson19@gmail.com>
2025-07-01 20:01:31 -0700

    Don't hardcode barrier id

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -2064,9 +2064,7 @@ class GPUModelRunner(LoRAModelRunnerMixin):
                 device_id=self.device.index,
                 # TODO: This worker id won't be unique for multi-node.
                 worker_id=self.device.index,
-                # Hardcode for now.
                 dtype_width_bytes=self.kv_cache_dtype.itemsize,
-                barrier_id="kvbm"
             )   
 
     def get_kv_cache_spec(self) -> dict[str, KVCacheSpec]:
